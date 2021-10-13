c441cc9a8a84a14ad7ef2da262a8cedf9e5b5c01
strugglehonor <2537731609@qq.com>
2021-10-13 15:59:18 +0800

    add DB settings

--- conf/conf.go
+++ conf/conf.go
@@ -1,7 +1,11 @@
 package conf
 
+// DB Settings
 var (
+	DBUser = "root"
+	DBPassWd = "Su948949$"
+	DBName = "video_server"
 	DBHost = "localhost"
 	Port = 3306
-	Charset = "utf-8"
+	Charset = "utf8"
 )
