8e2caf8edb0d1866c97355bfbedaf5db77429ce9
strugglehonor <2537731609@qq.com>
2021-12-22 22:52:32 +0800

    add logger

--- pkg/log/logger.go
+++ pkg/log/logger.go
@@ -1,7 +1,9 @@
 package log
 
 import (
+	"fmt"
 	"sync"
+	"os"
 )
 
 type logger struct {
@@ -19,4 +21,109 @@ func NewLogger(opts ...Option) *logger {
 			return new(Entry)
 		}},
 	}
+}
+
+func (l *logger) Debug(args ...interface{}) {
+	l.entry().write(DebugLevel, FmtEmptySeparate, args...)
+}
+
+func (l *logger) Info(args ...interface{}) {
+	l.entry().write(InfoLevel, FmtEmptySeparate, args...)
+}
+
+func (l *logger) Warn(args ...interface{}) {
+	l.entry().write(WarnLevel, FmtEmptySeparate, args...)
+}
+
+func (l *logger) Error(args ...interface{}) {
+	l.entry().write(ErrorLevel, FmtEmptySeparate, args...)
+}
+
+func (l *logger) Panic(args ...interface{}) {
+	l.entry().write(PanicLevel, FmtEmptySeparate, args...)
+	panic(fmt.Sprint(args...))
+}
+
+func (l *logger) Fatal(args ...interface{}) {
+	l.entry().write(FatalLevel, FmtEmptySeparate, args...)
+	os.Exit(1)
+}
+
+func (l *logger) Debugf(format string, args ...interface{}) {
+	l.entry().write(DebugLevel, format, args...)
+}
+
+func (l *logger) Infof(format string, args ...interface{}) {
+	l.entry().write(InfoLevel, format, args...)
+}
+
+func (l *logger) Warnf(format string, args ...interface{}) {
+	l.entry().write(WarnLevel, format, args...)
+}
+
+func (l *logger) Errorf(format string, args ...interface{}) {
+	l.entry().write(ErrorLevel, format, args...)
+}
+
+func (l *logger) Panicf(format string, args ...interface{}) {
+	l.entry().write(PanicLevel, format, args...)
+	panic(fmt.Sprintf(format, args...))
+}
+
+func (l *logger) Fatalf(format string, args ...interface{}) {
+	l.entry().write(FatalLevel, format, args...)
+	os.Exit(1)
+}
+
+// std logger
+func Debug(args ...interface{}) {
+	std.entry().write(DebugLevel, FmtEmptySeparate, args...)
+}
+
+func Info(args ...interface{}) {
+	std.entry().write(InfoLevel, FmtEmptySeparate, args...)
+}
+
+func Warn(args ...interface{}) {
+	std.entry().write(WarnLevel, FmtEmptySeparate, args...)
+}
+
+func Error(args ...interface{}) {
+	std.entry().write(ErrorLevel, FmtEmptySeparate, args...)
+}
+
+func Panic(args ...interface{}) {
+	std.entry().write(PanicLevel, FmtEmptySeparate, args...)
+	panic(fmt.Sprint(args...))
+}
+
+func Fatal(args ...interface{}) {
+	std.entry().write(FatalLevel, FmtEmptySeparate, args...)
+	os.Exit(1)
+}
+
+func Debugf(format string, args ...interface{}) {
+	std.entry().write(DebugLevel, format, args...)
+}
+
+func Infof(format string, args ...interface{}) {
+	std.entry().write(InfoLevel, format, args...)
+}
+
+func Warnf(format string, args ...interface{}) {
+	std.entry().write(WarnLevel, format, args...)
+}
+
+func Errorf(format string, args ...interface{}) {
+	std.entry().write(ErrorLevel, format, args...)
+}
+
+func Panicf(format string, args ...interface{}) {
+	std.entry().write(PanicLevel, format, args...)
+	panic(fmt.Sprintf(format, args...))
+}
+
+func Fatalf(format string, args ...interface{}) {
+	std.entry().write(FatalLevel, format, args...)
+	os.Exit(1)
 }
